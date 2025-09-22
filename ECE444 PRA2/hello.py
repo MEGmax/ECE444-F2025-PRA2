0378685c90603422b0f1228d3db46e15aac8ac74
Matthew Grech <matthewgrech@Matthews-MacBook-Pro-6.local>
2025-09-22 00:38:35 -0400

    activity 1.4- added email field

--- ECE444 PRA2/hello.py
+++ ECE444 PRA2/hello.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bleh bleh bleh'
@@ -13,20 +13,30 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your email address?', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        # Check if email contains 'utoronto'
+        if 'utoronto' not in form.email.data.lower():
+            flash('Please use a UofT email address!')
+            return render_template('index.html', form=form, name=session.get('name'))
+        
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('Activity1_3.html', name=name)
 
+
+if __name__ == '__main__':
+    app.run(debug=True)
